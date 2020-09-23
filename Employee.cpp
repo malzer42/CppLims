7e44deb45b2a0ab40bdc4232bfb81f43a2c47294
malzer42 <pmulamba@gmail.com>
2020-09-22 21:30:37 -0400

    Add files via upload

---
+++ Employee.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Employee.cpp
+ * Author: pamulamba
+ * 
+ * Created on 29 juin 2020, 19 h 39
+ */
+
+#include "Employee.h"
+
+Employee::Employee() {
+}
+
+Employee::Employee(const Employee& orig) {
+}
+
+Employee::~Employee() {
+}
+
---
+++ Employee.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Employee.h
+ * Author: pamulamba
+ *
+ * Created on 29 juin 2020, 19 h 39
+ */
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+class Employee {
+public:
+    Employee();
+    Employee(const Employee& orig);
+    virtual ~Employee();
+private:
+
+};
+
+#endif /* EMPLOYEE_H */
+
